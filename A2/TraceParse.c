493b3ff90c0deffde6f83f698317e1e4b82f95f6
Zev Isert <zev.isert@gmail.com>
2016-02-19 14:02:27 -0800

    Validate multiple input files

--- A2/TraceParse.c
+++ A2/TraceParse.c
@@ -1,8 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "TraceParse.h"
+
+#define VERBOSE 1
+
+/* I want booleans! */
+typedef enum { true, false } bool;
 
 int main(int count, char** args)
 {
-    return EXIT_FAILURE;
+    /*Check input args*/
+    count = verify_input(count, args);
+    if (count == 0) quit("No valid input files");
+    
+    quit("Source code not complete");
+    return EXIT_SUCCESS;
+}
+
+void quit(char* message)
+{
+    if (message != NULL)
+    {
+        printf("\n%s\n", message);
+    }
+    else printf("\n");
+
+    printf("Exiting...\n");
+    exit(EXIT_FAILURE);
+}
+
+int verify_input(int count, char** args)
+{
+    /* Skip program name */
+    count--; args++;
+
+    int numFiles = count;
+    int i = 0;
+    char** argPtr = args;
+    char* ext = NULL;
+    
+    bool shift = false;
+    
+    /* While there's more input strings */ 
+    while (*argPtr && strlen(*argPtr))
+    {
+        /* From right to left, find the first '.' in the string */
+        if( (ext = strrchr(*argPtr, '.')) != NULL)
+        {
+            /* If everything after the point above is .cap */
+            if(strcmp(ext, ".cap") == 0)
+            {
+                /* Good, input file ends in .cap */
+                FILE* file = fopen(*argPtr, "r");
+                if (file == NULL)
+                {
+                    printf("ERR: File %s doesn't exist, or doesn't have read permissions\n", *argPtr);
+                    shift = true;
+                }
+                /* Just checking if the file exists, we're done now */
+                else fclose(file);
+            }
+            /* The remainder of the string wasn't .cap */
+            else
+            {
+               if (VERBOSE) printf("ERR: File %s is not a .cap file.\n", *argPtr);
+               shift = true;
+            }
+        }
+        /* The whole string didn't have a '.' in it */
+        else
+        {
+            if (VERBOSE) printf("ERR: File %s has no extension.\n", *argPtr);
+            shift = true;
+        }
+        
+        /* The input we tested wasn't a .cap file */ 
+        if (shift == true)
+        {
+            shift = false;
+            char** currString = argPtr;
+            int j = i;
+            /* From the current point on, copy strings down an index */ 
+            for (++currString; *currString; ++currString, ++j)
+            {
+                args[j] = *currString;
+            }
+            /* Nullify the last string */
+            args[j] = "\0";
+
+            /* Decrease the number of valid input files */ 
+            numFiles -= 1;
+        }
+        /* Didn't shift the input array down, look at the next string */
+        else
+        {
+            if (VERBOSE) printf("File %s exists\n", *argPtr);
+            ++argPtr;
+            ++i;
+        }
+    }
+    return numFiles;
 }
--- A2/TraceParse.h
+++ A2/TraceParse.h
@@ -1,3 +1,5 @@
-
+/* Header file for TraceParse.c */
 
 int main(int, char**);
+void quit(char*);
+int verify_input(int, char**);
